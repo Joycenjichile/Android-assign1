bcee53c67cc80ab50c8ceea40de529993e96c547
joycenjichile <joycenkimalantoi@gmail.com>
2022-05-08 23:27:29 +0300

    first commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnBmi:Button
+    lateinit var btnSend:Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnBmi = findViewById(R.id.btnBmi)
+        btnBmi.setOnClickListener {
+            val  intent = Intent(this,BMICalculate::class.java)
+            startActivity(intent)
+        }
+        btnSend = findViewById(R.id.btnSend)
+        btnSend.setOnClickListener {
+            val  intent = Intent(this,sendMoney::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
